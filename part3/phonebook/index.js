a3e4dc5c99b0adacc1f17274c6b395c460d33449
patrycjakalinska <kalinska@student.agh.edu.pl>
2022-09-27 13:58:11 +0200

    deploy and frontend build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
-// app.use(morgan("tiny"));
+app.use(cors());
+
 morgan.token("body", function (req, res) {
   return req.body;
 });
@@ -20,6 +22,7 @@ const logger = morgan(function (tokens, req, res) {
   ].join(" ");
 });
 app.use(logger);
+app.use(express.static("build"));
 
 let persons = [
   {
@@ -102,7 +105,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
