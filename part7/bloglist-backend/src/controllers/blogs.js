39948eebc9c9e2914ee539918d63ce14ba19dbc4
Patrycja Kalinska <cysiak0412@gmail.com>
2023-04-27 16:41:56 +0200

    fix with blogs created by users

--- part7/bloglist-backend/src/controllers/blogs.js
+++ part7/bloglist-backend/src/controllers/blogs.js
@@ -52,6 +52,9 @@ blogsRouter.post('/', async (req, res, next) => {
     }
     const savedBlog = await blog.save()
 
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     res.status(201).json(savedBlog)
   } catch (err) {
     next(err)
