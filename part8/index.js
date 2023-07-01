a5762068097f3891c49cc8d962ff082877d0de54
Patrycja Kalinska <cysiak0412@gmail.com>
2023-07-01 12:34:57 +0200

    add 8.7

--- part8/index.js
+++ part8/index.js
@@ -125,6 +125,10 @@ const typeDefs = `
       published: Int
       genres:[String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -157,6 +161,15 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+
+      if (!author) return null
+
+      const updatedBook = { ...author, born: args.setBornTo }
+      authors.map((a) => (a.name === args.name ? updatedBook : a))
+      return updatedBook
     },
   },
 }
