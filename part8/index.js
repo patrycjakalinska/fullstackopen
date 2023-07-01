4183619860ade59b7f4f8ba2143076bc0373c557
Patrycja Kalinska <cysiak0412@gmail.com>
2023-07-01 12:05:57 +0200

    add 8.4

--- part8/index.js
+++ part8/index.js
@@ -4,26 +4,26 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 let authors = [
   {
     name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
     born: 1952,
   },
   {
     name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
+    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
+    born: 1963,
   },
   {
     name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
+    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
+    born: 1821,
   },
-  { 
+  {
     name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
   },
-  { 
+  {
     name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
   },
 ]
 
@@ -39,57 +39,57 @@ let authors = [
  * Spanish:
  * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
  * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
-*/
+ */
 
 let books = [
   {
     title: 'Clean Code',
     published: 2008,
     author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
+    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring'],
   },
   {
     title: 'Agile software development',
     published: 2002,
     author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
+    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
+    genres: ['agile', 'patterns', 'design'],
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
     author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
+    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring'],
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
     author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
+    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring', 'patterns'],
+  },
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
     author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
+    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring', 'design'],
   },
   {
     title: 'Crime and punishment',
     published: 1866,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
+    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
+    genres: ['classic', 'crime'],
   },
   {
     title: 'The Demon ',
     published: 1872,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
+    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
+    genres: ['classic', 'revolution'],
   },
 ]
 
@@ -101,7 +101,8 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]
+    allBooks(author: String!): [Book!]!
+    allAuthors: [Author!]!
   }
   type Book {
     title: String!
@@ -110,14 +111,24 @@ const typeDefs = `
     genres: [String!]
     id: ID!
   }
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
-  }
+    allBooks: (root, args) => books.filter((b) => b.author === args.author),
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter((b) => b.author === root.name).length,
+  },
 }
 
 const server = new ApolloServer({
